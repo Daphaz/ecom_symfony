43132f4e6eb7ed4820bf1d1be3d76adc95c294b7
Daphaz <bonnet.damien@ymail.com>
2021-04-17 01:17:40 +0200

    feat(payment): change style and sort order dashboard
    fix price order / 100

--- src/Controller/Admin/OrderCrudController.php
+++ src/Controller/Admin/OrderCrudController.php
@@ -4,6 +4,7 @@ namespace App\Controller\Admin;
 
 use App\Entity\Order;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
@@ -24,6 +25,11 @@ class OrderCrudController extends AbstractCrudController
         ->add('index','detail');
     }
 
+    public function configureCrud(Crud $crud): Crud
+    {
+      return $crud->setDefaultSort(['id' =>  'DESC']);
+    }
+
     public function configureFields(string $pageName): iterable
     {
         return [
@@ -31,6 +37,7 @@ class OrderCrudController extends AbstractCrudController
             DateTimeField::new('createdAt', 'Crée le'),
             TextField::new('user.getfullName', 'Client'),
             MoneyField::new('total')->setCurrency('EUR'),
+            MoneyField::new('carrierPrice','frais de port')->setCurrency('EUR'),
             BooleanField::new('isPaid','Payée'),
         ];
     }
